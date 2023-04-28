84857c7514df15d39d1fbfc20cd288ded339471d
jueun11 <jrangkim11@gmail.com>
2023-04-28 11:44:07 +0900

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let Btn = document.getElementById("beBtn");
 Btn.addEventListener('click', function () {
     console.log('click');
-    histroy.go(-1);
+    location.href="https://blog.naver.com/goldmedia8456";
 })
